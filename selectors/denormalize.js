c5aba70e60aef3e652b5e65055e4bc6e8d2b21b0
Andy Coenen <andy@patreon.com>
2016-12-02 12:21:53 -0800

    Update name in denormalize function

--- selectors/denormalize.js
+++ selectors/denormalize.js
@@ -2,27 +2,29 @@ import get from 'lodash.get'
 import set from 'lodash.set'
 import map from 'lodash.map'
 
-export default function denormalize(ref, entities, bag = {}) {
+export default function denormalize(ref, entities, existingObjects = {}) {
     if (!(ref && ref.type && ref.id)) {
         return undefined
     }
 
     const { type, id } = ref
 
-    const existing = get(bag, [type, id])
-    if (existing) {
-        return existing
+    // Check the existing object to see if a reference to the denormalized object already exists,
+    // if so, use the existing denormalized object
+    const existingObject = get(existingObjects, [type, id])
+    if (existingObject) {
+        return existingObject
     }
 
     const entity = get(entities, [type, id])
 
+    // Construct the new base denormalized object and add it to the existing map
     const obj = {
         id,
         type,
         ...entity.attributes
     }
-
-    set(bag, [type, id], obj)
+    set(existingObjects, [type, id], obj)
 
     const relationships = get(entity, 'relationships', {})
     map(relationships, (relationship, key) => {
@@ -30,10 +32,10 @@ export default function denormalize(ref, entities, bag = {}) {
         if (refOrRefs === null) {
             obj[key] = null
         } else if (!Array.isArray(refOrRefs)) {
-            obj[key] = denormalize(refOrRefs, entities, bag)
+            obj[key] = denormalize(refOrRefs, entities, existingObjects)
         } else {
             obj[key] = refOrRefs.map((ref) => { // eslint-disable-line
-                return denormalize(ref, entities, bag)
+                return denormalize(ref, entities, existingObjects)
             })
         }
     })
