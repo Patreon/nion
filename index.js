7abcf8dd7b2fca2c9450b141c9467085bb155c85
David Kettler <21echoes@gmail.com>
2017-02-06 15:37:32 -0800

    pull buildUrl out of the primary nion exports, because there’s no way for nion to know the api host without injection. add it back in via a nion wrapper in the Patreon codebase.

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Set up a `buildUrl` function for dynamic hostnames and terser api path construction in nion
+import { urlBuilderForDefaults } from 'utilities/json-api-url'
+export const buildUrl = urlBuilderForDefaults({include: []})
+
+// inject `buildUrl` as an option into the nion decorator
+import decoratorWithoutBuildURL from 'libs/nion-os'
+const decoratorWithBuildURL = (declarations = {}, options = {}) => decoratorWithoutBuildURL(declarations, {buildUrl, ...options})
+export default decoratorWithBuildURL
+
+export * from 'libs/nion-os'
