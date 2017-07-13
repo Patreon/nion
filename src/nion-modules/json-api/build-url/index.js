f1d7136b6d8ecf7fd3c1a9e3187ad75d5375d4d0
Jason Byttow <jasonbyttow@gmail.com>
2017-07-13 11:35:42 -0700

    removes unused dependendcy

--- src/nion-modules/json-api/build-url/index.js
+++ src/nion-modules/json-api/build-url/index.js
@@ -1,5 +1,4 @@
 import urlFactory from 'url-factory'
-import get from 'lodash.get'
 
 // eventually make this configurable via nion api-module config interface
 const CURRENT_JSON_API_VERSION = '1.0'
