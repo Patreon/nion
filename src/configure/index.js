3a46b01183888794b2695001222752275172305c
Jason Byttow <jasonbyttow@gmail.com>
2017-07-17 12:42:42 -0700

    removes safeguard check

--- src/configure/index.js
+++ src/configure/index.js
@@ -4,12 +4,6 @@ import ApiManager from '../api'
 import map from 'lodash.map'
 
 export default ({ apiModules, defaultApi, csrfProvider, defaultApiUrl }) => {
-    if (Object.keys(apiModules).length === 0) {
-        throw new Error(
-            'nion requires at least one API module. Please supply an API module in nion.configureNion',
-        )
-    }
-
     if (apiModules) {
         map(apiModules, (apiModule, name) => {
             ApiManager.registerApi(name, apiModule)
