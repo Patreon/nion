bc8839140eac8ba3d9e43358f676d14a89da35d8
Andy Coenen <andy@patreon.com>
2017-09-19 11:56:30 -0700

    Add in basic GET action unit tests

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -70,5 +70,55 @@ describe('nion: actions', () => {
                 expect(expectedAction).toEqual(obvservedActions[index])
             })
         })
+
+        it('GET api action should handle errors with the correct actions ', async () => {
+            const apiEndpoint = new ApiEndpoint()
+            const jsonPayload = { body: {} }
+
+            nock(apiEndpoint.baseUrl)
+                .get(apiEndpoint.route)
+                .reply(500, jsonPayload)
+
+            const dataKey = 'items'
+            const expectedActions = [
+                {
+                    type: actionTypes.NION_API_REQUEST,
+                    meta: {
+                        dataKey,
+                        endpoint: apiEndpoint.url,
+                        method: 'GET',
+                    },
+                },
+                {
+                    type: actionTypes.NION_API_FAILURE,
+                    meta: {
+                        dataKey,
+                        endpoint: apiEndpoint.url,
+                        method: 'GET',
+                    },
+                },
+            ]
+
+            const store = mockStore({})
+
+            try {
+                await store.dispatch(
+                    apiActions.get(dataKey, {
+                        endpoint: apiEndpoint.url,
+                    }),
+                )
+            } catch (err) {
+                // TODO: Get this working with the ApiManager.getErrorClass class constructor
+                expect(err).toBeInstanceOf(Error)
+                // Replace the error in the expectedAction payload with the Error caught here
+                // to simulate the logic of the payload construction
+                expectedActions[1].payload = err
+            }
+
+            const obvservedActions = store.getActions()
+            expectedActions.forEach((expectedAction, index) => {
+                expect(expectedAction).toEqual(obvservedActions[index])
+            })
+        })
     })
 })
