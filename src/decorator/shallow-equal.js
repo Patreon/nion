2258aef55589f874a09882b26e073a2b33f20055
Andy Coenen <andy@patreon.com>
2017-09-21 13:26:47 -0700

    Finalize denorm / internal audit

---
+++ src/decorator/shallow-equal.js
@@ -0,0 +1,44 @@
+// shallow equal from fbjs
+// https://github.com/reactjs/react-redux/blob/master/src/utils/shallowEqual.js
+// modified to ignore the "nion" prop
+
+const hasOwn = Object.prototype.hasOwnProperty
+
+function is(x, y) {
+    if (x === y) {
+        return x !== 0 || y !== 0 || 1 / x === 1 / y
+    } else {
+        return x !== x && y !== y
+    }
+}
+
+export default function shallowEqual(objA, objB) {
+    if (is(objA, objB)) return true
+
+    if (
+        typeof objA !== 'object' ||
+        objA === null ||
+        typeof objB !== 'object' ||
+        objB === null
+    ) {
+        return false
+    }
+
+    const keysA = Object.keys(objA)
+    const keysB = Object.keys(objB)
+
+    if (keysA.length !== keysB.length) return false
+
+    for (let i = 0; i < keysA.length; i++) {
+        const key = keysA[i]
+        if (key === 'nion') {
+            continue
+        }
+
+        if (!hasOwn.call(objB, key) || !is(objA[key], objB[key])) {
+            return false
+        }
+    }
+
+    return true
+}
