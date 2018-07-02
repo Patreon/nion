ce00c6509492e901e00bc37178091676302e8c8a
Ben Bayard <bjbayard@gmail.com>
2018-07-02 15:33:43 -0700

    Enable TSC compiler

--- test/setup.js
+++ test/setup.js
@@ -3,5 +3,7 @@ import raf from './polyfills' // eslint-disable-line no-unused-vars
 const Enzyme = require('enzyme')
 const EnzymeAdapter = require('enzyme-adapter-react-16')
 
+// Disable test warnings
+
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() })
